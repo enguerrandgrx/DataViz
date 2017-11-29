66fdeae2a4b33cdbc6bea366bbdabf1697ed2035
rutagara <maxime.rutag@epfl.ch>
2017-11-29 16:53:46 +0100

    first map

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-const width = 1100;
-const height = 700;
+//Width and height of map
+var width = 960;
+var height = 500;
 
 // D3 Projection
-const projection = d3.geoNaturalEarth1()
-	.rotate([0, 0])
-	.center([-95.7, 37.1])
-	.scale(16000)
-	.translate([width / 2, height / 2])
-	.precision(.1);
+var projection = d3.geoAlbersUsa()
+				   .translate([width/2, height/2])    // translate to center of screen
+				   .scale([1000]);          // scale things down so see entire US
 
-// path generator to convert JSON to SVG paths
-const path = d3.geoPath()
-	.projection(projection);
-
-//colormap for population density
-const color = d3.scaleLog()
-	.range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"])
-	.interpolate(d3.interpolateHcl);
+// Define path generator
+var path = d3.geoPath()               // path generator that will convert GeoJSON to SVG paths
+		  	 .projection(projection);  // tell path generator to use albersUsa projection
 
 const svg = d3.select("body")
-	.append("svg")
-	.attr("width", width)
-	.attr("height", height);
+					.append("svg")
+					.attr("width", width)
+					.attr("height", height);
 
 const div = d3.select("body")
-	.append("div")
-	.style("opacity", 0);
+					.append("div")
+					.style("opacity", 0);
 
-d3.json("https://raw.githubusercontent.com/enguerrandgrx/DataViz/master/topojson/countries/USA.json", function(json) {
-	// Map instagram posts
-	d3.csv("locations.csv", function(data) {
-			svg.selectAll("circle")
-				.data(data)
-				.enter()
-				.append("circle")
-				.attr("cx", (d) => projection([d.lon, d.lat])[0])
-				.attr("cy", (d) => projection([d.lon, d.lat])[1])
-				.attr("r", 5)
-				.style("fill", "#FF0000")
-				.style("opacity", 0.05)
-		});
+d3.csv("massshootings.csv", function(data) {
+	d3.json("usa.json", function(states) {
+
+		for(let i = 0; i < states.features.length; i++) {
+			let jsonState = states.features[i].properties.name;
+
+		}
+
+		svg.selectAll("path")
+			.data(states.features)
+			.enter()
+			.append("path")
+			.attr("d", path)
+			.style("fill", "rgb(213,222,217)")
+
+		//svg.selectAll("circle")
+		//	.data(data)
+
+
 	});
+});
